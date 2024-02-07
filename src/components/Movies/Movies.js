4dcc1335f3bd17ed3e5c894ff80d339f178125b6
Tamir Erdyneev <tamir431104@gmail.com>
2024-02-07 17:19:18 +0300

    level-2 (6.0)

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,11 +1,11 @@
 import './Movies.css'
-import MoviesCard from './MoviesCard/MoviesCard'
+import MoviesCardList from './MoviesCardList/MoviesCardList'
 import SearchForm from './SearchForm/SearchForm'
 export default function Movies(){
     return (
         <main className='movies'>
             <SearchForm/>
-            <MoviesCard/>
+            <MoviesCardList/>
         </main>
     )
 }
--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -1,14 +1,29 @@
 import './MoviesCard.css'
+import ok from '../../../images/icon__COLOR_invisible.svg'
 import card from '../../../images/pic__COLOR_pic.png';
-export default function MoviesCard(){
+import React from 'react';
+export default function MoviesCard(props){
+    const [Active, setActive] = React.useState(true);
+    const buttonClass = `movies-card__button ${Active ? 'movies-card__button_active' : ''}`
+    function changeButton(){
+        if(Active){
+            setActive(false);
+        } else{
+            setActive(true);
+        }
+
+    }
     return(
         <div className='movies-card'>
             <div className='movies-card__info'>
-                <h2 className='movies-card__name'>В погоне за Бенкси</h2>
-                <p className='movies-card__duration'>0ч 42м</p>
+                <h2 className='movies-card__name'>{props.name}</h2>
+                <p className='movies-card__duration'>{props.duration}</p>
             </div>
             <img src={card} className='movies-card__image' alt='Фотография из фильма'/>
-            <button className='movies-card__button'>Сохранить</button>
+            <button className={buttonClass} onClick={changeButton}>
+                {!Active? 'Сохранить' : 
+                <img src={ok} alt='Галочка'/>}
+                </button>
         </div>
     )
 }
