b34f23286b42b1ebc5754b41a5ebea9ffda73f2f
WebKit372 <69135250+WebKit372@users.noreply.github.com>
2024-02-14 23:36:22 +0300

    level-2 (10.10)

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -4,14 +4,14 @@ export default function MoviesCardList(props){
     return (
         <main className='moviescardlist'>
             <div className='moviescardlist__cards'>
-                <MoviesCard type={props.type} image={props.image} name='Абракадабра' duration='12'/>
-                <MoviesCard type={props.type} image={props.image} name='Абракадабра' duration='12'/>
-                <MoviesCard type={props.type} image={props.image} name='Абракадабра' duration='12'/>
-                <MoviesCard type={props.type} image={props.image} name='Абракадабра' duration='12'/>
-                <MoviesCard type={props.type} image={props.image} name='Абракадабра' duration='12'/>
-                <MoviesCard type={props.type} image={props.image} name='Абракадабра' duration='12'/>
-                <MoviesCard type={props.type} image={props.image} name='Абракадабра' duration='12'/>
-                <MoviesCard type={props.type} image={props.image} name='Абракадабра' duration='12'/>
+                <MoviesCard type={props.type} image={props.image} name='В погоне за Бенкси' duration='0ч 42м'/>
+                <MoviesCard type={props.type} image={props.image} name='В погоне за Бенкси' duration='0ч 42м'/>
+                <MoviesCard type={props.type} image={props.image} name='В погоне за Бенкси' duration='0ч 42м'/>
+                <MoviesCard type={props.type} image={props.image} name='В погоне за Бенкси' duration='12'/>
+                <MoviesCard type={props.type} image={props.image} name='В погоне за Бенкси' duration='12'/>
+                <MoviesCard type={props.type} image={props.image} name='В погоне за Бенкси' duration='12'/>
+                <MoviesCard type={props.type} image={props.image} name='В погоне за Бенкси' duration='12'/>
+                <MoviesCard type={props.type} image={props.image} name='В погоне за Бенкси' duration='12'/>
             </div>
             {props.isOverFilled?
                 <button className='moviescardlist__button'>Ещё</button> :
