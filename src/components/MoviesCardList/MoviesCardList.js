aca259ac7caf839d3806f67b227baaf7fd1917ee
Tamir Erdyneev <tamir431104@gmail.com>
2024-02-15 13:48:12 +0300

    level-2(fin.2)

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,8 +1,13 @@
 import './MoviesCardList.css'
 import MoviesCard from '../MoviesCard/MoviesCard'
+import Preloader from '../Preloader/Preloader'
 export default function MoviesCardList(props){
     return (
         <main className='moviescardlist'>
+            {props.loaded?
+                <Preloader/>:
+                ''
+            }
             <div className='moviescardlist__cards'>
                 <MoviesCard type={props.type} image={props.image} name='В погоне за Бенкси' duration='0ч 42м'/>
                 <MoviesCard type={props.type} image={props.image} name='В погоне за Бенкси' duration='0ч 42м'/>
